15e8424b3829466d99fa36aeedfb434f012f5a2c
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2024-11-28 23:03:26 -0500

    [feat] - Initial commit Frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold text-center"> Todo App </h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
